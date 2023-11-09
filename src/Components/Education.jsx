5bd2787197a93004df2122ce2e03b47b1012197b
MichaelOmoniyi <101010436+MichaelOmoniyi@users.noreply.github.com>
2023-11-10 00:03:59 +0100

    Create Education.jsx

---
+++ src/Components/Education.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
+            <p>Unversity of Lagos</p>
+        </section>
+    )
+}
+
+export default Education;
